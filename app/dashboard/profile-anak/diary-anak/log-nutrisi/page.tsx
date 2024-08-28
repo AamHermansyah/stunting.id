6c09fe69e49cd0fc603a2c3c1e599e2e0a646da7
pandupangestu <pangestup60@gmail.com>
2024-08-28 09:09:58 +0700

    [ADD] - Nutri Pribadi ASI Only

---
+++ app/dashboard/profile-anak/diary-anak/log-nutrisi/page.tsx
@@ -0,0 +1,13 @@
+import TabsNavigations from "@/app/dashboard/_components/tabs-navigation"
+import Case1 from "./_components/case-1"
+
+const page = () => {
+  return (
+    <>
+        <TabsNavigations />
+        <Case1/>
+    </>
+  )
+}
+
+export default page
