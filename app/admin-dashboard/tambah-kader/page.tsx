4c7848444f0ea87e032b73ed9edb047b311a3c34
pandupangestu <pangestup60@gmail.com>
2024-09-16 15:14:32 +0700

    [ADD] - Form Tambah Kader

---
+++ app/admin-dashboard/tambah-kader/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormAddKader from './_components/form-add-kader'
+
+const TambahKaderPage = () => {
+  return (
+    <>
+      <FormAddKader/>
+    </>
+  )
+}
+
+export default TambahKaderPage
