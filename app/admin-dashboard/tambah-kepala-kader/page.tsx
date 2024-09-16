b070ae4262799c0fb5739ebc47b4081041bf99be
pandupangestu <pangestup60@gmail.com>
2024-09-16 15:20:39 +0700

    [ADD] - Form Tambah Kepala Kader

---
+++ app/admin-dashboard/tambah-kepala-kader/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormAddKepalaKader from './_components/form-add-kepalakader'
+
+const TambahKepalaKaderPage = () => {
+  return (
+    <>
+      <FormAddKepalaKader/>
+    </>
+  )
+}
+
+export default TambahKepalaKaderPage
