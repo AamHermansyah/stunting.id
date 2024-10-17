4b0ebb7fceb6e103cba5c079b80449212d34a6dd
EgaAprianto <egaaprianto04@gmail.com>
2024-10-17 15:44:38 +0700

    [UPDATE] Detail Child Profile & delete child profile

--- app/management/orang-tua/profile/detail-anak/page.tsx
+++ app/management/orang-tua/profile/detail-anak/page.tsx
@@ -1,4 +1,4 @@
-import ProfileAnakPage from '@/app/dashboard/profile-anak/page'
+import ProfileAnakPage from '@/app/dashboard/profile-anak/[id]/page'
 import React from 'react'
 
 const page = () => {
