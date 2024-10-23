6cf19d976571072bd460b6c7673293410fe3ebf9
EgaAprianto <egaaprianto04@gmail.com>
2024-10-23 12:49:52 +0700

    [UPDATE] - Search Parrent

--- data/user.ts
+++ data/user.ts
@@ -1,8 +1,16 @@
 import { prisma } from "@/db";
 
-export const getAllUsers = async () => {
+export const getAllUsers = async (searchQuery: string | null = null) => {
   try {
     const users = await prisma.user.findMany({
+      where: searchQuery
+        ? {
+            name: {
+              contains: searchQuery,
+              mode: 'insensitive', // Agar pencarian tidak case-sensitive
+            },
+          }
+        : {}, // Jika tidak ada query, ambil semua data
       include: {
         _count: {
           select: { Child: true },
