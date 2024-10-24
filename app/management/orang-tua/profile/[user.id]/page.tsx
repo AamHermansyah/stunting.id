42870c3071ee71e434449b3fb65173c332fc851b
EgaAprianto <egaaprianto04@gmail.com>
2024-10-24 11:13:53 +0700

    [UPDATE] - Profile Parrent file structure

--- app/management/orang-tua/profile/[user.id]/page.tsx
+++ app/management/orang-tua/profile/[user.id]/page.tsx
@@ -1,20 +1,19 @@
+
 import ChildrenProfile from '@/app/dashboard/(home)/_components/children-profile'
 import { AuthCookie } from '@/types';
 import { cookies } from 'next/headers';
-import { useParams } from 'next/navigation';
 import React from 'react'
 
 const ProfilePage = () => {
   const authCookie = cookies().get('auth');
   const user = JSON.parse(authCookie!.value) as AuthCookie;
-  const id = useParams();
 
   return (
     <div>
       <ChildrenProfile
         id={user.id}
-        detail={`/management/orang-tua/profile/${id}/detail-anak`}
-        add={`/management/orang-tua/profile/${id}/tambah-anak`}
+        detail={`/management/orang-tua/profile/${user.id}/detail-anak`}
+        add={`/management/orang-tua/profile/${user.id}/tambah-anak`}
       />
     </div>
   )
