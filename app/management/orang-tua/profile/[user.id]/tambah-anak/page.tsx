932c028ff7064fe599e02e0968c8f14d87627ada
EgaAprianto <egaaprianto04@gmail.com>
2024-11-17 20:49:51 +0700

    [FIX] Bug Form Add Child Profile

--- app/management/orang-tua/profile/[user.id]/tambah-anak/page.tsx
+++ app/management/orang-tua/profile/[user.id]/tambah-anak/page.tsx
@@ -1,17 +1,17 @@
-import FormAddAnak from '@/components/shared/form-add-anak'
-import { AuthCookie } from '@/types';
-import { cookies } from 'next/headers';
-import React from 'react'
+"use client";
+
+import FormAddAnak from '@/components/shared/form-add-anak';
+import { useParams } from 'next/navigation';
 
 const AddChildrenPage = () => {
-  const authCookie = cookies().get('auth');
-  const user = JSON.parse(authCookie!.value) as AuthCookie;
+  const params = useParams();
+  const parentId = params['user.id'];
 
   return (
     <div>
-      <FormAddAnak id={user.id} />
+      <FormAddAnak id={parentId as string} />
     </div>
-  )
-}
+  );
+};
 
-export default AddChildrenPage
+export default AddChildrenPage;
