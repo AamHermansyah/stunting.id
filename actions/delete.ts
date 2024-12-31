1356535f47b89ba1ae8ddcf1950bd4451329b9ac
Pandu Pangestu <pangestup60@gmail.com>
2024-12-31 07:19:49 +0700

    [UPDATE] - Feature Delete Parent

--- actions/delete.ts
+++ actions/delete.ts
@@ -1,44 +1,66 @@
-'use server'
+'use server';
 
 import { prisma } from "@/db";
 
 export const deleteUser = async (userId: string) => {
   try {
-    // Cari data orang tua berdasarkan ID
+
     const user = await prisma.user.findUnique({
       where: { id: userId },
-      include: { Child: true }, // Sertakan anak-anak yang terkait
+      include: {
+        Child: {
+          include: {
+            NutritionLog: {
+              include: {
+                mealNutrition: true,
+                breastfeedingTimes: true,
+              },
+            },
+          },
+        },
+      },
     });
 
-    // Jika data orang tua tidak ditemukan
     if (!user) {
-      return { error: 'Orang tua tidak ditemukan' };
+      return { error: 'User not found' };
     }
 
-    // Hapus semua pengukuran terkait untuk setiap anak
     for (const child of user.Child) {
+      for (const log of child.NutritionLog) {
+
+        await prisma.mealNutrition.deleteMany({
+          where: { nutritionLogId: log.id },
+        });
+
+        await prisma.breastfeedingTime.deleteMany({
+          where: { nutritionLogId: log.id },
+        });
+      }
+
+      await prisma.nutritionLog.deleteMany({
+        where: { childId: child.id },
+      });
+
       await prisma.measurement.deleteMany({
         where: { childId: child.id },
       });
     }
 
-    // Hapus semua anak terkait
     await prisma.child.deleteMany({
       where: { userId: userId },
     });
 
-    // Hapus data orang tua
     await prisma.user.delete({
       where: { id: userId },
     });
 
     return {
-      success: 'Orang tua berhasil dihapus',
+      success: 'User deleted successfully',
     };
   } catch (error) {
     console.error("Error deleting user:", error);
     return {
-      error: 'Terjadi kesalahan pada server',
+      error: 'Server error occurred',
     };
   }
-}; 
+};
